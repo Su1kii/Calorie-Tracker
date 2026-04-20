0957fd49264f55de47b6abb66882e75dd1935789
Steven Echeverria <stevennava749@gmail.com>
2026-04-20 11:45:41 -0600

    Add project pause note in CalorieTrackerApplication
    Commented out a note about pausing the project until next month.

--- src/main/java/dev/stev/calorie_tracker/CalorieTrackerApplication.java
+++ src/main/java/dev/stev/calorie_tracker/CalorieTrackerApplication.java
@@ -2,10 +2,10 @@ package dev.stev.calorie_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Need to focus on other project, Will start this project up again next month 5/01/2026
 @SpringBootApplication
 public class CalorieTrackerApplication {
-
+								
 	public static void main(String[] args) {
 		SpringApplication.run(CalorieTrackerApplication.class, args);
 	}
