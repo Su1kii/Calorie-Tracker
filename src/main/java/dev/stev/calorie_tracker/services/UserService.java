64245d495f3cfd2d4b61c0d1c7f41c0953d2ac9f
Steven Echeverria <stevennava749@gmail.com>
2026-05-20 14:59:36 -0600

    Delete src directory

--- src/main/java/dev/stev/calorie_tracker/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package dev.stev.calorie_tracker.services;
-
-import dev.stev.calorie_tracker.domain.DTOs.UserCreateDTO;
-import dev.stev.calorie_tracker.domain.DTOs.UserResponseDTO;
-
-public interface UserService {
-    UserResponseDTO getUserById(Integer id);
-}
